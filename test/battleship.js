2b009e5151cd6d1613ed73ffb10404e6b634652d
Benoit Sagols <benoit.sagols@gmail.com>
2018-08-27 02:33:05 +0700

    Commented solidity tests

--- test/battleship.js
+++ test/battleship.js
@@ -4,8 +4,8 @@ const _shuffle = require("lodash/shuffle");
 var Battleship = artifacts.require("./Battleship.sol");
 
 contract("Battleship", async accounts => {
-  const account1 = accounts[0];
-  const account2 = accounts[1];
+  const playerA = accounts[0];
+  const playerB = accounts[1];
 
   const ships = [2, 2, 0, 0, 0, 0, 0, 0, 0];
 
@@ -17,63 +17,68 @@ contract("Battleship", async accounts => {
 
   let gameId = -1;
 
+  // Simple test for Player A  to create a game
   it("...should create game", async () => {
     const sc = await Battleship.deployed();
 
     gameId++;
 
-    [secret, salt1] = _getSecrets(web3, account1, ships);
-    await sc.createGame(3, secret, { from: account1 });
+    [secret, salt1] = _getSecrets(web3, ships);
+    await sc.createGame(3, secret, { from: playerA });
 
     [status, gridSize, , owner, , turn] = await sc.games.call(gameId);
 
     assert.equal(status.toNumber(), 0, "Status not OPEN");
     assert.equal(gridSize.toNumber(), 3, "Wrong grid size");
-    assert.equal(owner, account1, "Owner address not saved");
+    assert.equal(owner, playerA, "Owner address not saved");
     assert.equal(turn, owner, "Wrong turn");
   });
 
+  // Simple test to make sure Player A cannot join its own game
   it("...should not join (own) game", async () => {
     const sc = await Battleship.deployed();
 
     try {
-      await sc.joinGame(gameId, "SHIPS", { from: account1 });
+      await sc.joinGame(gameId, "SHIPS", { from: playerA });
       assert(false, "Exception was not thrown");
     } catch (err) {
       assert(true, "Exception was thrown");
     }
   });
 
+  // Simple test to make sure Player B can join Player A open game
   it("...should join game", async () => {
     const sc = await Battleship.deployed();
 
-    [secret, salt2] = _getSecrets(web3, account2, ships);
+    [secret, salt2] = _getSecrets(web3, ships);
 
-    await sc.joinGame(gameId, secret, { from: account2 });
+    await sc.joinGame(gameId, secret, { from: playerB });
 
     [status, , , , challenger] = await sc.games.call(gameId);
 
-    assert.equal(challenger, account2, "Challenger address not saved");
+    assert.equal(challenger, playerB, "Challenger address not saved");
     assert.equal(status.toNumber(), 1, "Status not READY");
   });
 
+  // Simple test to make sure Player A can perform the intial attack of the game
   it("...should attack", async () => {
     const sc = await Battleship.deployed();
 
-    await sc.attack(gameId, 0, { from: account1 });
+    await sc.attack(gameId, 0, { from: playerA });
 
     [status, , targetIndex, , , turn] = await sc.games.call(gameId);
 
     assert.equal(targetIndex.toNumber(), 0, "Wrong attack index");
     assert.equal(status.toNumber(), 2, "Status not STARTED");
-    assert.equal(turn, account2, "Wrong turn");
+    assert.equal(turn, playerB, "Wrong turn");
   });
 
+  // Simple test to make sure Player A cannot play (not its turn)
   it("...should not be player turn", async () => {
     const sc = await Battleship.deployed();
 
     try {
-      await sc.counterAttack(gameId, 1, false, { from: account1 });
+      await sc.counterAttack(gameId, 1, false, { from: playerA });
 
       assert(false, "Exception was thrown");
     } catch (err) {
@@ -81,113 +86,120 @@ contract("Battleship", async accounts => {
     }
   });
 
+  // This test performs a "normal" game, with nobody cheating
   it("...should finish game", async () => {
     const sc = await Battleship.deployed();
 
-    await sc.counterAttack(gameId, 0, true, { from: account2 });
-    await sc.counterAttack(gameId, 1, true, { from: account1 });
-    await sc.counterAttack(gameId, 1, true, { from: account2 });
+    await sc.counterAttack(gameId, 0, true, { from: playerB });
+    await sc.counterAttack(gameId, 1, true, { from: playerA });
+    await sc.counterAttack(gameId, 1, true, { from: playerB });
 
     [status, , , , , , winner] = await sc.games.call(gameId);
 
     assert.equal(status.toNumber(), 3, "Status not FINISHED");
-    assert.equal(winner, account1, "Wrong winner");
+    assert.equal(winner, playerA, "Wrong winner");
   });
 
+  // This test reveals both players ships positions
+  // - Player A remains the winner – because no cheating (yet)
   it("...should be winning", async () => {
     const sc = await Battleship.deployed();
 
     await sc.reveal(gameId, ships.join(""), salt1, {
-      from: account1
+      from: playerA
     });
 
     await sc.reveal(gameId, ships.join(""), salt2, {
-      from: account2
+      from: playerB
     });
 
     [status, , , , , , winner] = await sc.games.call(gameId);
 
     assert.equal(status.toNumber(), 4, "Status not DONE");
-    assert.equal(winner, account1, "Wrong winner");
+    assert.equal(winner, playerA, "Wrong winner");
   });
 
+  // This test performs a "cheated" game, where only Player B is cheating
+  // In order to make sure winner is switched to Player A
   it("...should be cheating", async () => {
     const sc = await Battleship.deployed();
 
     gameId++;
 
-    [secret, salt1] = _getSecrets(web3, account1, ships);
-    await sc.createGame(3, secret, { from: account1 });
+    [secret, salt1] = _getSecrets(web3, ships);
+    await sc.createGame(3, secret, { from: playerA });
 
-    [secret, salt2] = _getSecrets(web3, account2, ships);
-    await sc.joinGame(gameId, secret, { from: account2 });
+    [secret, salt2] = _getSecrets(web3, ships);
+    await sc.joinGame(gameId, secret, { from: playerB });
 
-    // Account 2 (winning, but cheating)
-    await sc.attack(gameId, 0, { from: account1 });
-    await sc.counterAttack(gameId, 0, false, { from: account2 });
-    await sc.counterAttack(gameId, 1, true, { from: account1 });
-    await sc.counterAttack(gameId, 1, false, { from: account2 });
-    await sc.counterAttack(gameId, 2, true, { from: account1 });
+    // Player B (winning first, but cheating)
+    await sc.attack(gameId, 0, { from: playerA });
+    await sc.counterAttack(gameId, 0, false, { from: playerB });
+    await sc.counterAttack(gameId, 1, true, { from: playerA });
+    await sc.counterAttack(gameId, 1, false, { from: playerB });
+    await sc.counterAttack(gameId, 2, true, { from: playerA });
 
     [, , , , , , winner] = await sc.games.call(gameId);
 
-    assert.equal(winner, account2, "Wrong winner");
+    assert.equal(winner, playerB, "Wrong winner");
 
     await sc.reveal(gameId, ships.join(""), salt1, {
-      from: account1
+      from: playerA
     });
 
     await sc.reveal(gameId, ships.join(""), salt2, {
-      from: account2
+      from: playerB
     });
 
     [, , , , , , winner] = await sc.games.call(gameId);
 
-    assert.equal(winner, account1, "Wrong winner");
+    assert.equal(winner, playerA, "Wrong winner");
   });
 
+  // This test performs a "cheated" game, where both Players are cheating
+  // In order to make sure nobody wins
   it("...should be BOTH cheating", async () => {
     const sc = await Battleship.deployed();
 
     gameId++;
 
-    [secret, salt1] = _getSecrets(web3, account1, ships);
+    [secret, salt1] = _getSecrets(web3, ships);
 
-    await sc.createGame(3, secret, { from: account1 });
+    await sc.createGame(3, secret, { from: playerA });
 
-    [secret, salt2] = _getSecrets(web3, account2, ships);
+    [secret, salt2] = _getSecrets(web3, ships);
 
-    await sc.joinGame(gameId, secret, { from: account2 });
+    await sc.joinGame(gameId, secret, { from: playerB });
 
-    // Account 2 (winning, but cheating)
-    await sc.attack(gameId, 0, { from: account1 });
-    await sc.counterAttack(gameId, 0, false, { from: account2 });
-    await sc.counterAttack(gameId, 1, false, { from: account1 });
-    await sc.counterAttack(gameId, 1, false, { from: account2 });
-    await sc.counterAttack(gameId, 2, false, { from: account1 });
-    await sc.counterAttack(gameId, 2, false, { from: account2 });
-    await sc.counterAttack(gameId, 3, false, { from: account1 });
-    await sc.counterAttack(gameId, 3, false, { from: account2 });
-    await sc.counterAttack(gameId, 4, false, { from: account1 });
-    await sc.counterAttack(gameId, 4, false, { from: account2 });
-    await sc.counterAttack(gameId, 5, false, { from: account1 });
-    await sc.counterAttack(gameId, 5, false, { from: account2 });
-    await sc.counterAttack(gameId, 6, false, { from: account1 });
-    await sc.counterAttack(gameId, 6, false, { from: account2 });
-    await sc.counterAttack(gameId, 7, false, { from: account1 });
-    await sc.counterAttack(gameId, 7, false, { from: account2 });
+    // Player B (winning, but cheating)
+    await sc.attack(gameId, 0, { from: playerA });
+    await sc.counterAttack(gameId, 0, false, { from: playerB });
+    await sc.counterAttack(gameId, 1, false, { from: playerA });
+    await sc.counterAttack(gameId, 1, false, { from: playerB });
+    await sc.counterAttack(gameId, 2, false, { from: playerA });
+    await sc.counterAttack(gameId, 2, false, { from: playerB });
+    await sc.counterAttack(gameId, 3, false, { from: playerA });
+    await sc.counterAttack(gameId, 3, false, { from: playerB });
+    await sc.counterAttack(gameId, 4, false, { from: playerA });
+    await sc.counterAttack(gameId, 4, false, { from: playerB });
+    await sc.counterAttack(gameId, 5, false, { from: playerA });
+    await sc.counterAttack(gameId, 5, false, { from: playerB });
+    await sc.counterAttack(gameId, 6, false, { from: playerA });
+    await sc.counterAttack(gameId, 6, false, { from: playerB });
+    await sc.counterAttack(gameId, 7, false, { from: playerA });
+    await sc.counterAttack(gameId, 7, false, { from: playerB });
 
     [status, , , , , , winner] = await sc.games.call(gameId);
 
     assert.equal(status.toNumber(), 3, "Not FINISHED");
-    assert.equal(winner, account1, "Wrong winner");
+    assert.equal(winner, playerA, "Wrong winner");
 
     await sc.reveal(gameId, ships.join(""), salt1, {
-      from: account1
+      from: playerA
     });
 
     await sc.reveal(gameId, ships.join(""), salt2, {
-      from: account2
+      from: playerB
     });
 
     [, , , , , , winner] = await sc.games.call(gameId);
@@ -200,8 +212,9 @@ contract("Battleship", async accounts => {
   });
 });
 
-function _getSecrets(web3, account, ships) {
-  const salt = web3.eth.sign(account, _shuffle(ships).join(""));
+function _getSecrets(web3, ships) {
+  // const salt = web3.eth.sign(account, _shuffle(ships).join(""));
+  const salt = web3.sha3(_shuffle(ships).join(""));
   const secret = web3.sha3(ships.join("") + salt);
 
   return [secret, salt];
